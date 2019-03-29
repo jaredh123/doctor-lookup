03cd8c1018b1916c566e329cfc1dac1adb513657
jaredh123 <jared.hanson12345@gmail.com>
2019-03-29 16:54:28 -0700

    Add basics to project document

---
+++ src/doctorLookup.js
@@ -0,0 +1,9 @@
+import $ from 'jquery';
+
+export default class DoctorLookup {
+  constructor(doctorName, medicalIssue) {
+    this.doctorName = doctorName;
+    this.medicalIssue = medicalIssue;
+  }
+
+}
---
+++ src/main.js
@@ -0,0 +1,16 @@
+import DoctorLookup from "./doctorLookup.js";
+import $ from 'jquery';
+import 'bootstrap';
+import './styles.css';
+
+$(document).ready(function() {
+
+  
+  $("#run").submit(function(event) {
+    event.preventDefault();
+
+
+
+    $("#result").text();
+  });
+});
