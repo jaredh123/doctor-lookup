9d6ca3e71e4651b8aca61defd775afc14f3cba4e
jaredh123 <jared.hanson12345@gmail.com>
2019-04-08 08:10:40 -0700

    Add all functionality to project

--- src/doctorLookup.js
+++ src/doctorLookup.js
@@ -6,4 +6,30 @@ export default class DoctorLookup {
     this.medicalIssue = medicalIssue;
   }
 
+  findDoctors() {
+    return new Promise((resolve, reject) => {
+      let request = new XMLHttpRequest();
+      let url = "";
+      let that = this;
+      if (this.doctorName != "" && this.medicalIssue != "") {
+        url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${this.doctorName}&query=${that.condition}&location=or-portland&skip=0&user_key=${process.env.exports.apiKey}`;
+      }
+      else if (this.doctorName == "") {
+        url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${that.medicalIssue}&location=or-portland&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
+      }
+      else {
+        url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${this.doctorName}&location=or-portland&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
+      }
+      request.onload = function() {
+        if (this.status == 200) {
+          resolve(request.response);
+        }
+        else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    })
+  }
 }
--- src/main.js
+++ src/main.js
@@ -4,13 +4,32 @@ import 'bootstrap';
 import './styles.css';
 
 $(document).ready(function() {
-
-  
   $("#run").submit(function(event) {
     event.preventDefault();
 
+    let doctorName = $("#doctorName").val();
+    let medicalIssue = $("#medicalIssue").val();
 
+    let doctorLookup = new DoctorLookup(doctorName, medicalIssue);
 
-    $("#result").text();
+    let promise = doctorLookup.findDoctors();
+    $(".results").empty();
+    $(".result").show();
+    promise.then(function(response) {
+      let body = JSON.parse(response);
+      if (body.data.length === 0) {
+        $("#errors").append("No results");
+      }
+      else {
+        for (let i = 0; i < body.data.length; i++) {
+          if (body.data[i].practices.length > 0) {
+            $(".doctorInfo").append(`<div>` + `<p>Name: ${body.data[i].profile.first_name} ${body.data[i].profile.last_name}</p>` + `<p>Address: ${body.data[i].practices[0].visit_address.street}, ${body.data[i].practices[0].visit_address.street2}, ${body.data[i].practices[0].visit_address.city}, ${body.data[i].practices[0].visit_address.state}, ${body.data[i].practices[0].visit_address.zip}</p>` + `<p>Website: ${body.data[i].practices[0].website}</p>` + `<p>Accepts New Patients: ${body.data[i].practices[0].accepts_new_patients}</p>` + `</div>` + `<br>`);
+          }
+        }
+      }
+    }).catch((reason) => {
+      $("#error").empty();
+      $("#error").append(`${reason}`)
+    })
   });
 });
